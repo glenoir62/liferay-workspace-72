4f2e1238e9646f44508d9042c5e7d058cba2c636
Guillaume Lenoir <guillaume.lenoir@ext.leroymerlin.fr>
2020-11-04 01:04:00 +0100

    feat(openapi) add doc, update,

--- modules/cocktails/cocktail–ws-impl/src/main/java/fr/gleo/liferay/meetup/ws/internal/resource/v1_0/CocktailResourceImpl.java
+++ modules/cocktails/cocktail–ws-impl/src/main/java/fr/gleo/liferay/meetup/ws/internal/resource/v1_0/CocktailResourceImpl.java
@@ -26,62 +26,62 @@ import java.util.stream.Collectors;
  * @author Guillaume Lenoir
  */
 @Component(
-        properties = "OSGI-INF/liferay/rest/v1_0/cocktail.properties",
-        scope = ServiceScope.PROTOTYPE, service = CocktailResource.class
+		properties = "OSGI-INF/liferay/rest/v1_0/cocktail.properties",
+		scope = ServiceScope.PROTOTYPE, service = CocktailResource.class
 )
 public class CocktailResourceImpl extends BaseCocktailResourceImpl {
 
-    private final static Log LOGGER = LogFactoryUtil.getLog(CocktailResourceImpl.class);
+	private final static Log LOGGER = LogFactoryUtil.getLog(CocktailResourceImpl.class);
 
 
-    @Reference
-    private CompanyService companyService;
+	@Reference
+	private CompanyService companyService;
 
-    @Reference
-    private CocktailLocalService cocktailLocalService;
+	@Reference
+	private CocktailLocalService cocktailLocalService;
 
-    @Override
-    public void deleteCocktail(@NotNull Long cocktailId) throws Exception {
-        LOGGER.info("deleteCocktail");
+	@Override
+	public void deleteCocktail(@NotNull Long cocktailId) throws Exception {
+		LOGGER.info("deleteCocktail");
 		cocktailLocalService.deleteCocktail(cocktailId);
 
-        super.deleteCocktail(cocktailId);
-    }
+		super.deleteCocktail(cocktailId);
+	}
 
-    @Override
-    public Cocktail getCocktail(@NotNull Long cocktailId) throws Exception {
-        LOGGER.info("getCocktail");
-        return _toCocktailDTO(cocktailLocalService.getCocktail(cocktailId));
-    }
+	@Override
+	public Cocktail getCocktail(@NotNull Long cocktailId) throws Exception {
+		LOGGER.info("Get Cocktail test = " + cocktailId);
+		return _toCocktailDTO(cocktailLocalService.getCocktail(cocktailId));
+	}
 
-    @Override
-    public Cocktail postSiteCocktail(@NotNull Long siteId, Cocktail cocktail) throws Exception {
-        LOGGER.info("postSiteCocktail ");
+	@Override
+	public Cocktail postSiteCocktail(@NotNull Long siteId, Cocktail cocktail) throws Exception {
+		LOGGER.info("postSiteCocktail ");
 
 
-        long companyId = PortalUtil.getDefaultCompanyId();
-        Company company = companyService.getCompanyById(companyId);
-        long userId = company.getDefaultUser().getUserId();
-        long groupId = siteId;
+		long companyId = PortalUtil.getDefaultCompanyId();
+		Company company = companyService.getCompanyById(companyId);
+		long userId = company.getDefaultUser().getUserId();
+		long groupId = siteId;
 
-        ServiceContext serviceContext = new ServiceContext();
-        serviceContext.setUserId(userId);
-        serviceContext.setCompanyId(companyId);
-        serviceContext.setScopeGroupId(groupId);
+		ServiceContext serviceContext = new ServiceContext();
+		serviceContext.setUserId(userId);
+		serviceContext.setCompanyId(companyId);
+		serviceContext.setScopeGroupId(groupId);
 
-        return _toCocktailDTO(cocktailLocalService.addCocktail(groupId, cocktail.getName(), cocktail.getDescription(), cocktail.getImage(), serviceContext));
-    }
+		return _toCocktailDTO(cocktailLocalService.addCocktail(groupId, cocktail.getName(), cocktail.getDescription(), cocktail.getImage(), serviceContext));
+	}
 
-    @Override
-    public Cocktail putCocktail(@NotNull Long cocktailId, Cocktail cocktail) throws Exception {
-        LOGGER.info("putCocktail " + cocktail);
+	@Override
+	public Cocktail putCocktail(@NotNull Long cocktailId, Cocktail cocktail) throws Exception {
+		LOGGER.info("putCocktail " + cocktail);
 
-        return _toCocktailDTO(cocktailLocalService.updateCocktail(cocktailId, cocktail.getName(), cocktail.getDescription(), cocktail.getImage(), null));
-    }
+		return _toCocktailDTO(cocktailLocalService.updateCocktail(cocktailId, cocktail.getName(), cocktail.getDescription(), cocktail.getImage(), null));
+	}
 
-    @Override
-    public Page<Cocktail> getSiteCocktailsPage(@NotNull Long siteId, String search, Filter filter, Pagination pagination, Sort[] sorts) throws Exception {
-        LOGGER.info("getSiteCocktailsPage");
+	@Override
+	public Page<Cocktail> getSiteCocktailsPage(@NotNull Long siteId, String search, Filter filter, Pagination pagination, Sort[] sorts) throws Exception {
+		LOGGER.info("Get Site Cocktails Page = " + siteId);
 
 		List<Cocktail> cocktails = cocktailLocalService.getCocktailsByGroupId(siteId)
 				.stream()
@@ -89,22 +89,22 @@ public class CocktailResourceImpl extends BaseCocktailResourceImpl {
 				.collect(Collectors.toList());
 
 		return Page.of(cocktails);
-    }
+	}
 
-    private Cocktail _toCocktailDTO(fr.gleo.meetup.liferay.model.Cocktail cocktail) {
+	private Cocktail _toCocktailDTO(fr.gleo.meetup.liferay.model.Cocktail cocktail) {
 
-        if (cocktail == null) {
-            return null;
-        }
+		if (cocktail == null) {
+			return null;
+		}
 
-        return new Cocktail() {
-            {
-                name = cocktail.getName();
-                createDate = cocktail.getCreateDate();
-                image = cocktail.getImage();
-                id = cocktail.getCocktailId();
-                description = cocktail.getDescription();
-            }
-        };
-    }
+		return new Cocktail() {
+			{
+				name = cocktail.getName();
+				createDate = cocktail.getCreateDate();
+				image = cocktail.getImage();
+				id = cocktail.getCocktailId();
+				description = cocktail.getDescription();
+			}
+		};
+	}
 }
